df5abb33eeae47eae04fcfe63ff95f73e25eaed2
Kevin Wallace <kevjava@gmail.com>
2015-02-28 15:00:37 -0600

    First working version of the Game of Life.  Spawns 300 random cells.

--- src/gameoflife/GameOfLife.java
+++ src/gameoflife/GameOfLife.java
@@ -7,33 +7,56 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class GameOfLife extends BasicGame {
-	
+
 	private static Logger logger = Logger.getLogger(GameOfLife.class.getName());
+	private static final int INITIAL_TICK_TIME = 250; // milliseconds
+	private static final int _tickTime = INITIAL_TICK_TIME;
+	private static long _lastTick = 0;
+	private GameOfLifeLogic _logic;
+	private GridView _gridView;
 
 	public GameOfLife(String gameName) {
 		super(gameName);
 	}
 
 	@Override
-	public void init(GameContainer arg0) throws SlickException {
+	public void init(GameContainer gc) throws SlickException {
+		_logic = new GameOfLifeLogic(gc.getWidth(), gc.getHeight());
+		_logic.init();
+		_gridView = new GridView(_logic.getCellsWide(), _logic.getCellsHigh(), _logic.getCellWidth(),
+				_logic.getCellHeight(), gc.getWidth(), gc.getHeight());
+		_lastTick = System.currentTimeMillis();
 	}
 
 	@Override
 	public void render(GameContainer gc, Graphics graphics) throws SlickException {
-		graphics.drawString("Howdy!", 100, 100);
+		float tickPercentage = ((float) (System.currentTimeMillis() - _lastTick) / (float) _tickTime);
+		_gridView.draw(graphics, _logic.getCellGrid(), tickPercentage);
 	}
 
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
-		
+	public void update(GameContainer gc, int delta) throws SlickException {
+		Input input = gc.getInput();
+		if (input.isKeyPressed(Input.KEY_ESCAPE)) {
+			gc.exit(); // Quick escape.
+		}
+
+		long timeSinceLastTick = System.currentTimeMillis() - _lastTick;
+		if (timeSinceLastTick > _tickTime) {
+			_logic.tick();
+			_lastTick += _tickTime;
+		}
 	}
 
 	/**
-	 * THE BEGINNING OF ALL THINGS (incipio omnium rerum)
-	 * @param args Command-line arguments.
+	 * Life begins.
+	 * 
+	 * @param args
+	 *            Command-line arguments.
 	 */
 	public static void main(String... args) {
 		try {
